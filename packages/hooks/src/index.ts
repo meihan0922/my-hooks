87086f26c4b3e9b55d112700e952db2eb810c589
mei huang <meihen0922@gmail.com>
2026-01-27 14:29:32 +0800

    feat(packages/hooks): ✨  add state hooks: useToggle

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,10 +1,1 @@
-// import { add, divide, multiply, subtract } from './math';
-import { TestVersion } from '@my-hooks/core';
-
-export interface User {
-  name: string;
-}
-
-console.log('%c packages/hooks/src/index.ts:8 ', 'color: #007acc;', TestVersion);
-
-// console.log('%c packages/hooks/index.js:1 hooks', 'color: #007acc;');
+export { useToggle } from './useToggle';
---
+++ packages/hooks/src/useToggle/index.ts
@@ -0,0 +1,28 @@
+import { useMemo, useState } from 'react';
+
+/**
+ * Toggle between a boolean or any two values.
+ *
+ * @param defaultValue - Initial value. Default is false.
+ * @param reverseValue - Toggle value. If not provided, it will be the opposite of the default value.
+ * @returns [currentValue, { toggle: Fn, set: Fn }]
+ **/
+export function useToggle<T, R>(
+  defaultValue: T = false as unknown as T,
+  reverseValue?: R,
+): [T | R, { toggle: () => void; set: (value: T | R) => void }] {
+  const [state, setState] = useState<T | R>(defaultValue);
+
+  const actions = useMemo(() => {
+    const reverseValueOriginal = reverseValue ?? !defaultValue;
+    return {
+      toggle: () =>
+        setState(prev =>
+          prev === defaultValue ? (reverseValueOriginal as unknown as R) : (defaultValue as unknown as T),
+        ),
+      set: (value: T | R) => setState(value),
+    };
+  }, []);
+
+  return [state, actions];
+}
